a2df9475bc564e3a81c6c54859eaf6c076270203
S-cheptoo <cheptoosandra.t@gmail.com>
2021-10-06 11:07:09 +0300

    Initial commit

---
+++ src/main/java/com/myproject/tracom/SpringWithJwtApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.tracom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWithJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWithJwtApplication.class, args);
+    }
+
+}
